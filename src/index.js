baaf3ed30ed64f3ae2986266f6b427fb48c36eaf
Gerald Gorman <gpgorman158@gmail.com>
2022-02-08 23:02:59 -0500

    attempted to add a dropdown with color priority

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
   let form = document.querySelector('form')
   form.addEventListener('submit', function(e) {
     e.preventDefault();
-    console.log(e);
+    //console.log(e);
     //console.log(e.target['new-task-description'].value);
-    handleTask(e.target.querySelector('#new-task-description').value);
+    buildTask(e.target.querySelector('#new-task-description').value);
     form.reset();
     //let submitValue = e.target.querySelector('#new-task-description').value;
     //document.querySelector('#tasks').append(submitValue);
@@ -14,22 +14,47 @@ document.addEventListener("DOMContentLoaded", function() {
   
 });
 
-function handleTask (task) {
+function buildTask (task) {
   let list = document.querySelector('#tasks')
   let btn = document.createElement('button');
+  //delete event listener
   btn.addEventListener('click', handleDelete);
   let listElement = document.createElement('li');
-  btn.textContent = 'x'
+  
+  let priority = document.createElement('select');
+  let highPrior = document.createElement('option');
+  highPrior.value = "high";
+  highPrior.textContent = 'High';
+  highPrior.style.color = 'red';
+  let lowPrior = document.createElement('option');
+  lowPrior.value = "low";
+  lowPrior.textContent = 'Low';
+  lowPrior.style.color = 'green';
+  
+  btn.textContent = 'x';
   list.appendChild(listElement);
-  listElement.append(`${task} `, btn);
+  listElement.append(`${task} `, priority, btn);
+  priority.append(highPrior, lowPrior);
   //console.log(task);
+  //updateBColor();
 }
 
 function handleDelete(e){
   e.target.parentNode.remove();
 
 }
-
-
+/*
+function updateBColor(){
+  let liArray = document.querySelectorAll('li');
+  console.log(liArray);
+  Array.from(liArray);
+  console.log(liArray);
+  for(element in liArray){
+    console.log(element.value);
+    if (element.value === document.getElementsByTagName[0].value)
+    {  return "this is bogus"; } 
+  };
+}
+*/
 
 
