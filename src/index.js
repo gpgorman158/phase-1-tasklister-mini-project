26fd3ea9f0069f6c586c154c2f499ac4540a3723
Gerald Gorman <gpgorman158@gmail.com>
2022-02-08 21:45:03 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", function() {
+  let form = document.querySelector('form')
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    console.log(e);
+    //console.log(e.target['new-task-description'].value);
+    handleTask(e.target.querySelector('#new-task-description').value);
+    form.reset();
+    //let submitValue = e.target.querySelector('#new-task-description').value;
+    //document.querySelector('#tasks').append(submitValue);
+
+    
+  })
+  
 });
+
+function handleTask (task) {
+  let list = document.querySelector('#tasks')
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  let listElement = document.createElement('li');
+  btn.textContent = 'x'
+  list.appendChild(listElement);
+  listElement.append(`${task} `, btn);
+  //console.log(task);
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+
+}
+
+
+
+
